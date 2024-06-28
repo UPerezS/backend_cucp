23cd88bee45aa30fb2ddc289caacc63a4b5ef338
UPerezS <cama8836@gmail.com>
2024-06-28 11:35:38 -0600

    cucp manejo de rutas

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,44 @@
-//app.ts
-import express from 'express';
-const app = express();
- 
-/*  DotEnv  */
-const dotenv = require('dotenv');
-dotenv.config();
-const port = process.env.PORT || 3000;
- 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+import express, { Application } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+//import authRoutes from './routes/authRoutes';
 
-app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
-});
-  
+class Server {
+    private app: Application;
+
+    //Inicializa la clase
+    constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+        this.app.listen(this.app.get("port"), () => {
+            console.log("Server on port", this.app.get("port"));
+        });
+    }
+
+    //Configuracion de modulos
+    config(): void {
+        // configuración del puerto para el servidor
+        this.app.set("port", 3000);
+
+        // muestra las peticiones en consola
+        this.app.use(morgan("dev"));
+
+        // puertos de conexión de la API
+        this.app.use(cors());
+
+        // solo se permiten peticiones en formato JSON
+        this.app.use(bodyParser.json());
+
+        this.app.use(bodyParser.urlencoded({ extended: false, }));
+    }
+
+    //Configura las rutas
+    routes() {
+        //this.app.use("/", authRoutes);
+        //this.app.use("/usuarios", usuariosRoutes);
+    }
+}
+
+const server = new Server();
---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,20 @@
+import {Router, RouterOptions} from 'express';
+//import { authController } from '../controllers/authControllers';
+
+class AuthRoutes {
+    // Objeto de tipo router
+    public router: Router;
+
+    // Inicializa
+    constructor(){
+        this.router = Router();
+        this.config();
+    }
+
+    config(){
+        //this.router.post('/', authController.iniciarSesion);
+    }
+}
+
+const authRoutes = new AuthRoutes();
+export default authRoutes.router;   
