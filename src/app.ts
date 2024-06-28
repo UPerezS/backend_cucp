a1ab07bc1cf81adfa0989a2ea6007214d3b71ea2
UPerezS <cama8836@gmail.com>
2024-06-28 11:14:49 -0600

    cucp Creación Servidor

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+//app.ts
+import express from 'express';
+const app = express();
+ 
+/*  DotEnv  */
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 3000;
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${port}`);
+});
+  
